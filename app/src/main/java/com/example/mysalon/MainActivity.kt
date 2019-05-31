2b4aeaea77446500ca44462f7f11215bf592a333
Cherry <masahiro970513@gmail.com>
2019-05-31 16:11:36 +0900

    スライド起動

--- app/src/main/java/com/example/mysalon/MainActivity.kt
+++ app/src/main/java/com/example/mysalon/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.mysalon
 
 import android.os.Bundle
+import android.os.Handler
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+//スライド
+    class MyAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+        private val resources = listOf(
+            R.drawable.salon1,R.drawable.salon2,
+            R.drawable.salon3,R.drawable.salon4)
 
+        override fun getCount(): Int {
+            return  resources.size
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return  ImageFragment.newInstance(resources[position])
+        }
+    }
+//end スライド
     private lateinit var textMessage: TextView
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -33,5 +53,13 @@ class MainActivity : AppCompatActivity() {
 
         textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        pager.adapter = MyAdapter(supportFragmentManager)
+        val handler = Handler()
+        timer(period = 4500){
+            handler.post{
+                pager.currentItem = (pager.currentItem+1)%4
+            }
+        }
     }
 }
